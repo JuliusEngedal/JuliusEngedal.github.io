33239e97991d05214682d801dad3bc62f49efc70
JuliusE <julius.engedal@gmail.com>
2022-10-12 08:54:32 +0200

    Galleri

--- Galleri/src/java.js
+++ Galleri/src/java.js
@@ -1,17 +1,24 @@
-/* // When the user scrolls the page, execute myFunction
+/* === Nav start === */
+
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+/* Sticky */
+
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
-} */
+}
+
+
+/* === Nav slut === */
